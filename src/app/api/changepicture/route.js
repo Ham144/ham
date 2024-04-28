1123bb21145186c6a46690f629374c4954cdca3a
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-28 23:40:52 +0700

    i still have no time  today

--- src/app/api/changepicture/route.js
+++ src/app/api/changepicture/route.js
@@ -1,5 +1,7 @@
 export async function POST(req) {
     const data = await req.FormData()
-    console.log(data);
-    Response.json("Ok")
+    const file = data.get("file")
+    if (file.length === 1) {
+        Response.json("Ok")
+    }
 }
