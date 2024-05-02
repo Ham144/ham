408dd14b8350d5158119a59e383965056e220c12
ham144 <24434muhammad.yafizham@gmail.com>
2024-05-02 07:06:58 +0000

    May 2, 2024, 2:06 PM

--- src/app/api/changepicture/route.js
+++ src/app/api/changepicture/route.js
@@ -1,7 +1,5 @@
 export async function POST(req) {
     const data = await req.FormData()
     const file = data.get("file")
-    if (file.length === 1) {
-        Response.json("Ok")
-    }
+    console.log(file)
 }
