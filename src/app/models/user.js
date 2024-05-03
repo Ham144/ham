ad9ed681a15c4e565714e114e84c4e65ee27bdc7
ham144 <24434muhammad.yafizham@gmail.com>
2024-05-03 23:37:11 +0000

    May 4, 2024, 6:37 AM

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -11,35 +11,7 @@ const userSchema = new Schema({
         required: true,
         unique: true
     },
-    phone: {
-        type: String,
-        required: true,
-        default: ""
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    country: {
-        type: String,
-        required: true,
-        default: ""
-    },
-    postalCode: {
-        type: String,
-        required: true,
-        default: ""
-    },
-    city: {
-        type: String,
-        required: true,
-        default: ""
-    },
-    specificAddress: {
-        type: String,
-        required: true,
-        default: ""
-    }
+
 }, { timestamps: true })
 
 
