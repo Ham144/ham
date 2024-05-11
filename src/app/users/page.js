24ef59d1592993542e5d9892f66f169fc2684b10
ham144 <24434muhammad.yafizham@gmail.com>
2024-05-11 16:20:00 +0000

    May 11, 2024, 11:20 PM

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import ProfileBar from '../components/ProfileBar'
+import useProfile from '../components/UseProfile'
 
 const UsersPage = () => {
+    const [users, setUsers] = useState([])
+
+    const { loading, data } = useProfile()
+    if (!loading) {
+        console.log(data)
+    }
+
     return (
         <div className='px-4'>
             <ProfileBar isAdmin={true} />
-            UsersPage</div>
+
+
+        </div>
     )
 }
 
