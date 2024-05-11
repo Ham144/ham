b90b7ed68bcee7c610b919b3dc7dc096c8c865d1
ham144 <24434muhammad.yafizham@gmail.com>
2024-05-11 23:26:28 +0700

    ntah

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,20 +1,37 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProfileBar from '../components/ProfileBar'
 import useProfile from '../components/UseProfile'
+import toast from 'react-hot-toast'
+import { FaExternalLinkAlt } from 'react-icons/fa'
 
 const UsersPage = () => {
     const [users, setUsers] = useState([])
 
-    const { loading, data } = useProfile()
-    if (!loading) {
-        console.log(data)
+    const getAllUsers = async () => {
+        const usersPromise = (async (resolve, reject) => {
+            const response = await fetch("/api/profile")
+            const data = await response.json()
+            console.log(data);
+        })
+
+        await toast.promise(usersPromise, {
+            loading: "getting all users data",
+            success: "all users extracted",
+            error: "something wrong with exracting data"
+        })
     }
+
+    useEffect(() => {
+        getAllUsers()
+    }, [])
 
     return (
         <div className='px-4'>
             <ProfileBar isAdmin={true} />
-
+            <div>{
+                users
+            }</div>
 
         </div>
     )
