22a95f007557379fb4a384f5e2f483660bfc53db
ham144 <24434muhammad.yafizham@gmail.com>
2024-05-12 09:01:22 +0000

    May 12, 2024, 4:01 PM

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,23 +1,57 @@
 import { User } from "@/app/models/user";
+import { UserInfo } from "@/app/models/userInfo";
 import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { MdAutoAwesomeMosaic } from "react-icons/md";
+import { authOption } from "../auth/[...nextauth]/route";
 
-//get all users
 export async function GET() {
     mongoose.connect(process.env.MONGO_URL)
-    const data = []
-    const users = await User.find().lean()
-    const userinfos = await User.find().lean()
-    if (users.length <= 0) {
-        return Response.json({ status: 404, ok: false }, { message: "data user is less then 1" })
-    }
-
-    [users, userinfos].map((user, info) => {
-        if (user.email === info.email) {
-            data.push(...user, ...email)
+    const combined = []
+    const _user = await User.find().lean()
+    const userInfo = await UserInfo.find().lean()
+    _user.map((user) => {
+        const found = userInfo.findIndex(info => {
+            return info.email == user.email
+        })
+        if (found !== -1) {
+            combined.push({ ...user, ...userInfo[found] })
         }
         else {
-            data.push(user)
+            combined.push(user)
         }
     })
-    return Response.json(data)
+
+    return Response.json(combined);
+}
+
+export async function PATCH(req) {
+    const { email, isAdmin } = await req.json()
+    mongoose.connect(process.env.MONGO_URL)
+    try {
+        await UserInfo.findOneAndUpdate({ email }, {
+            isAdmin
+        })
+    } catch (error) {
+        throw new Error(error)
+        return Response.json({ status: 404, ok: false })
+    }
+    return Response.json({ ok: true })
+}
+
+export async function DELETE(req) {
+    //cek jika id atau email adalah milik user saat ini 
+    const { email } = await req.json()
+    mongoose.connect(process.env.MONGO_URL)
+    const session = await getServerSession(authOption)
+    const isMainAdmin = (session?.user?.email === "24434muhamamd.yafizham@gmail.com")
+    const isLoggedin = (session?.user?.email === email)
+
+    console.log(session?.user?.email, "24434muhamamd.yafizham@gamil.com")
+    if (isLoggedin || isMainAdmin) {
+        return Response.json({ ok: false, status: 401, message: "You cant delete this user because this is yourself or its the main admin" })
+    }
+    // await User.findOneAndDelete(email)
+    console.log("terhapus")
+    return Response.json({ ok: true, message: "deleted" })
 }
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -4,6 +4,7 @@ import ProfileBar from '../components/ProfileBar'
 import useProfile from '../components/UseProfile'
 import toast from 'react-hot-toast'
 import { FaExternalLinkAlt } from 'react-icons/fa'
+import Spinner from '../components/Spinner'
 
 const UsersPage = () => {
     const [users, setUsers] = useState([])
@@ -11,13 +12,52 @@ const UsersPage = () => {
     const getAllUsers = async () => {
         const usersPromise = fetch("/api/users")
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                setUsers(data)
+            })
+            .catch(err => console.log(err))
 
         await toast.promise(usersPromise, {
             loading: "getting all users data",
             success: "all users extracted",
             error: "something wrong with exracting data"
         })
+        console.log(users)
+    }
+
+    async function updateAdmin(email, isAdmin) {
+        const updatedIsadmin = !isAdmin
+
+        const response = await fetch("/api/users", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email, isAdmin: updatedIsadmin
+            })
+        })
+
+        if (response.ok) {
+            toast.success("Admin status updated")
+            getAllUsers()
+        }
+    }
+
+    async function handleDelete(email) {
+        const res = await fetch("/api/users", {
+            method: "DELETE",
+            body: JSON.stringify({
+                email
+            })
+        })
+        if (res.status === 401) {
+            console.log(res.message)
+        }
+        else if (res.ok) {
+            getAllUsers()
+            toast.success("success deleted")
+        }
     }
 
     useEffect(() => {
@@ -25,11 +65,40 @@ const UsersPage = () => {
     }, [])
 
     return (
-        <div className='px-4'>
+        <div className='flex px-4 '>
             <ProfileBar isAdmin={true} />
-            <div>{
+            <div className='flex flex-col-reverse justify-center mx-auto 
+            md:w-[50%] max-md:translate-x-7 mb-12 '>{
+                    users.length > 0 ? users.map((u) => (
+                        <div key={u._id} className='flex  border px-4 py-2 rounded-md mt-3 '>
+                            <div className='flex flex-col grow' >
+                                <h1 className='font-bold'>{u.name || <span className='text-slate-700 italic'>no name</span>}</h1>
+                                <p>{u.email || <span className='text-slate-700 italic'>no email</span>}</p>
+                                <div className='flex justify-start md:w-[500px] max-md:w-[100%] bg-slate-100 py-2 rounded-md px-2'>
+                                    <span className='border-r-4'>Created At: {u.createdAt}</span>
+                                    <span>updated At: {u.updatedAt}</span>
+                                </div>
+                                <div>
+                                    <p>phone: {u.phone || <span className='text-slate-700 italic'>not found</span>}</p>
+                                </div>
+                                <div className='flex'>
+                                    <p>city : {u.city || <span className='text-slate-700 italic'>not found</span>}</p>
+                                    <p className='font-bold'>, {u.country || <span className='text-slate-700 italic'> unknown country</span>}</p>
+                                </div>
+                                <div className='flex gap-2 hover:bg-slate-50 w-full' onClick={() => updateAdmin(u.email, u.isAdmin)}>
 
-            }</div>
+                                    <input type="checkbox" id={u._id} checked={u.isAdmin} />
+                                    <label htmlFor={u._id}>{u?.isAdmin ? "uncheck to make this user no longer be admin" : "Check to make this user admin"} </label>
+                                </div>
+                            </div>
+                            <div className='flex flex-col justify-center '>
+                                <button className='bg-sekunder hover:bg-slate-200'>edit</button>
+                                <button className='bg-sekunder text-red-50 hover:bg-slate-200' onClick={() => handleDelete(u.email)}>delete</button>
+                            </div>
+                        </div>
+                    )) :
+                        <div className='flex items-center h-screen justify-center'><Spinner /></div>
+                }</div>
 
         </div>
     )
