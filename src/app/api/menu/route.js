006ca11182556cda47af22b92e3a62394ee2f794
ham144 <24434muhammad.yafizham@gmail.com>
2024-05-15 23:38:22 +0000

    May 16, 2024, 6:38 AM

--- src/app/api/menu/route.js
+++ src/app/api/menu/route.js
@@ -1,6 +1,11 @@
+import { MenuItems } from "@/app/models/menuitems"
+import mongoose from "mongoose"
+
 export async function POST(req) {
     const url = req.url
     const searchString = url.split('=')[1]
-
+    mongoose.connect(process.env.MONGO_URL)
+    const categories = await req.json()
+    const response = await MenuItems.find({ name: searchString, categories: categories })
     return Response.json({ ok: true })
 }
