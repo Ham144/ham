0073291d783fdfdcd170f2b3e2dcc23ce3728dd9
ham144 <24434muhammad.yafizham@gmail.com>
2024-07-16 17:21:07 +0700

    complete look single product next

--- src/app/favorites/page.js
+++ src/app/favorites/page.js
@@ -5,12 +5,27 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import useUserinfosProduct from "../components/hooks/useUserinfosProduct";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 export default function FavoritesPage() {
 
     if (useSession().status == "unauthenticated") return redirect("/")
-    const { data, user } = useUserinfosProduct()
-    const favorites = data?.filter(item => item.isFavorite)
+    const { data, setRefresh } = useUserinfosProduct()
+    const [favorites, setFavorites] = useState()
+    const refresh = setRefresh
+
+    async function fetchingFavorites() {
+        if (!data) return
+        refresh()
+        const items = data.filter((item) => item.isFavorite === true)
+        setFavorites(items)
+    }
+
+    useEffect(() => {
+        fetchingFavorites()
+    }, [data, refresh, favorites])
+
     return (
         <div className="w-full min-h-screen flex flex-col items-center mx-auto">
             <h1 className="text-3xl p-4 gap-3 flex items-center italic text-primer font-light">
@@ -29,23 +44,24 @@ export default function FavoritesPage() {
                             <div className="card-body">
                                 <h1 className="card-title bg-orange-200 p-3 rounded-sm">Product Name : {item.name}</h1>
                                 <h2 className="card-title">Quantity in cart : {item.quantity}</h2>
-                                <h3 className="card-title">Base Price : {item.price}</h3>
+                                <h3 className="card-title">Base Price : {item.price}$</h3>
                                 <h3 className="card-title">in Cart Since : {item.addedDate}</h3>
                                 <p className="truncate">IDProduct-{item.menuItemId}</p>
                                 <div className="card-actions justify-between">
                                     <button className="btn btn-primary">Look</button>
                                     <label onClick={async () => {
                                         try {
-                                            await axios.get("/api/isfavorite", {
+                                            const data = await axios.post("/api/isfavorite", {
                                                 _id: item?.menuItemId,
                                                 isFavorite: !item?.isFavorite,
                                                 userInfos_id: item?.userInfos_id
                                             })
-
+                                            if (data.data.ok) toast.success(data.data.msg)
+                                            return fetchingFavorites()
                                         } catch (error) {
                                             console.log(error)
                                         }
-                                    }} className="label cursor-pointer">favorited:  <span className="label-text">{item.isFavorite}</span>
+                                    }} className="label cursor-pointer">remove from favorite?   <span className="label-text">{item.isFavorite}</span>
                                         <input type="checkbox" className="toggle" checked={item.isFavorite} />
                                     </label>
                                 </div>
