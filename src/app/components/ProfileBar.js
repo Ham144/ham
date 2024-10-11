eb97b4e7e7863aaea65ca559dd4cdddc46f6c3f6
ham144 <24434muhammad.yafizham@gmail.com>
2024-10-11 08:47:02 +0700

    menu items back to profile now is ok

--- src/app/components/ProfileBar.js
+++ src/app/components/ProfileBar.js
@@ -15,7 +15,7 @@ const ProfileBar = ({ isAdmin }) => {
         mt-3 
         md:gap-y-3
         z-30`}>
-            <Link href={"http://localhost:3000/profile/0"} className={`${path === "profile/0" ? "buble-menu-selected" : "buble-menu"}`}>Profile</Link>
+            <Link href={"/profile/0"} className={`${path === "profile/0" ? "buble-menu-selected" : "buble-menu"}`}>Profile</Link>
             <Link href={"/categories"} className={`${path === "categories" ? "buble-menu-selected" : "buble-menu"}`}>Categories</Link>
             <Link href={"/menu-items"} className={`${path === "menu-items" ? "buble-menu-selected" : "buble-menu"}`}>Menu Items</Link>
             <Link href={"/users"} className={`${path === "users" ? "buble-menu-selected" : "buble-menu"}`}>Users</Link>
